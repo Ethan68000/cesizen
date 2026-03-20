7dd2439f711f1440a84afc458a1fdeed081a4bbb
Ethan68000 <cathycoulon910@gmail.com>
2026-03-20 20:26:41 +0100

    feat: export/import JSON exercices de respiration

--- src/Controller/ExerciceRespirationController.php
+++ src/Controller/ExerciceRespirationController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/exercice/respiration')]
@@ -85,5 +86,89 @@ final class ExerciceRespirationController extends AbstractController
         return $this->render('exercice_respiration/launch.html.twig', [
             'exercice_respiration' => $exerciceRespiration,
         ]);
+    }
+
+    // -------------------------------------------------------
+    // EXPORT JSON
+    // -------------------------------------------------------
+    #[Route('/export/json', name: 'app_exercice_respiration_export', methods: ['GET'])]
+    public function export(ExerciceRespirationRepository $repo): Response
+    {
+        $exercices = $repo->findAll();
+        $data = [];
+
+        foreach ($exercices as $exercice) {
+            $data[] = [
+                'nameSeries'      => $exercice->getNameSeries(),
+                'timeInspiration' => $exercice->getTimeInspiration(),
+                'timeApnea'       => $exercice->getTimeApnea(),
+                'timeExpiration'  => $exercice->getTimeExpiration(),
+            ];
+        }
+
+        $response = new JsonResponse($data);
+        $response->headers->set('Content-Disposition', 'attachment; filename="exercices.json"');
+
+        return $response;
+    }
+
+    // -------------------------------------------------------
+    // IMPORT JSON
+    // -------------------------------------------------------
+    #[Route('/import/json', name: 'app_exercice_respiration_import', methods: ['POST'])]
+    public function import(Request $request, EntityManagerInterface $em): Response
+    {
+        $uploadedFile = $request->files->get('json_file');
+
+        if (!$uploadedFile || $uploadedFile->getClientOriginalExtension() !== 'json') {
+            $this->addFlash('danger', 'Veuillez uploader un fichier JSON valide.');
+            return $this->redirectToRoute('app_exercice_respiration_index');
+        }
+
+        $content = file_get_contents($uploadedFile->getPathname());
+        $data = json_decode($content, true);
+
+        if (!is_array($data)) {
+            $this->addFlash('danger', 'Le fichier JSON est invalide.');
+            return $this->redirectToRoute('app_exercice_respiration_index');
+        }
+
+        $count = 0;
+        foreach ($data as $item) {
+            if (!isset($item['nameSeries'], $item['timeInspiration'], $item['timeApnea'], $item['timeExpiration'])) {
+                continue;
+            }
+
+            $exercice = new ExerciceRespiration();
+            $exercice->setNameSeries($item['nameSeries']);
+            $exercice->setTimeInspiration((int) $item['timeInspiration']);
+            $exercice->setTimeApnea((int) $item['timeApnea']);
+            $exercice->setTimeExpiration((int) $item['timeExpiration']);
+            $em->persist($exercice);
+            $count++;
+        }
+
+        $em->flush();
+        $this->addFlash('success', $count . ' exercice(s) importé(s) avec succès !');
+
+        return $this->redirectToRoute('app_exercice_respiration_index');
+    }
+
+    #[Route('/{id}/export', name: 'app_exercice_respiration_export_one', methods: ['GET'])]
+    public function exportOne(ExerciceRespiration $exerciceRespiration): Response
+    {
+        $data = [
+            [
+                'nameSeries'      => $exerciceRespiration->getNameSeries(),
+                'timeInspiration' => $exerciceRespiration->getTimeInspiration(),
+                'timeApnea'       => $exerciceRespiration->getTimeApnea(),
+                'timeExpiration'  => $exerciceRespiration->getTimeExpiration(),
+            ]
+        ];
+
+        $response = new JsonResponse($data);
+        $response->headers->set('Content-Disposition', 'attachment; filename="exercice_' . $exerciceRespiration->getId() . '.json"');
+
+        return $response;
     }
 }
