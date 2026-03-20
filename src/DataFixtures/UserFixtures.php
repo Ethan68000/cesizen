223d6fe100d1461760bdb22806ee62ca56452055
Ethan68000 <cathycoulon910@gmail.com>
2026-03-20 18:38:31 +0100

    feat : mise en place crud + entite user

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {}
+
+    public function load(ObjectManager $manager): void
+    {
+        // Admin
+        $admin = new User();
+        $admin->setPseudo('admin');
+        $admin->setEmail('admin@gmail.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setCreationDate(new \DateTime());
+        $admin->setPassword($this->hasher->hashPassword($admin, 'AdminAdmin12+'));
+        $manager->persist($admin);
+
+        // User simple
+        $user = new User();
+        $user->setPseudo('ethan');
+        $user->setEmail('ethan@gmail.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setCreationDate(new \DateTime());
+        $user->setPassword($this->hasher->hashPassword($user, 'UserEthan12+'));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
