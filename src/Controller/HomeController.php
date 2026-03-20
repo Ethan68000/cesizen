29b9ba40bc5169a7a775f0680fbf17d85cc6d114
Ethan68000 <cathycoulon910@gmail.com>
2026-03-20 20:39:02 +0100

    feat : v1 design

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ExerciceRespirationRepository;
+use App\Repository\InformationsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig');
+    public function index(
+        InformationsRepository $informationsRepository,
+        ExerciceRespirationRepository $exerciceRespirationRepository
+    ): Response {
+        return $this->render('home/index.html.twig', [
+            'informations' => $informationsRepository->findAll(),
+            'exercices'    => $exerciceRespirationRepository->findBy([], null, 3),
+        ]);
     }
 }
